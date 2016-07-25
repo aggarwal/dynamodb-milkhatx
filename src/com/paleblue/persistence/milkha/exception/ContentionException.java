e09da8ba40c57935975e2307ea95ab9d6d3e21b3
Shitanshu Aggarwal <saggarwal.90@gmail.com>
2016-07-24 23:57:23 -0700

    exploding the recently open-sourced milkha tarball

---
+++ src/com/paleblue/persistence/milkha/exception/ContentionException.java
@@ -0,0 +1,16 @@
+package com.paleblue.persistence.milkha.exception;
+
+
+public class ContentionException extends RuntimeException {
+    public ContentionException() {
+        super();
+    }
+
+    public ContentionException(String message) {
+        super(message);
+    }
+
+    public ContentionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/com/paleblue/persistence/milkha/exception/InvalidTransactionStateTransitionException.java
@@ -0,0 +1,16 @@
+package com.paleblue.persistence.milkha.exception;
+
+
+public class InvalidTransactionStateTransitionException extends RuntimeException {
+    public InvalidTransactionStateTransitionException() {
+        super();
+    }
+
+    public InvalidTransactionStateTransitionException(String message) {
+        super(message);
+    }
+
+    public InvalidTransactionStateTransitionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
